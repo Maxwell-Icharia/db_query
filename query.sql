850631611948447bf935b5d02602f63f9b56b0a5
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-02 12:43:49 +0300

    [Add] - SQL Query

---
+++ query.sql
@@ -0,0 +1,23 @@
+SELECT 
+    `stop_name`
+FROM
+    `stops`
+WHERE
+    `stop_name` != 'Ngong'
+
+ORDER BY `stop_name` ASC;
+
+
+SELECT 
+	`frequencies`.`start_time`,
+	 `frequencies`.`end_time`
+FROM
+    `frequencies`
+
+ORDER BY `frequencies`.`start_time` ASC;
+
+SELECT 
+	(`frequencies`.`end_time` - `frequencies`.`start_time`) AS `duration`,
+    `frequencies`.`start_time`, `frequencies`.`end_time`
+FROM
+    `frequencies`
